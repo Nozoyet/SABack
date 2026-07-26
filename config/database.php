95c200b00bcd009571254f42493ababa731185a6
Nozomi <nozomi.callisaya@gmail.com>
2026-07-26 14:16:45 -0400

    Fix database.php

--- config/database.php
+++ config/database.php
@@ -60,8 +60,10 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+        // Configuración para permitir SSL en Aiven sin exigir un archivo .crt físico
+        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+    ]) : [],
         ],
 
         'mariadb' => [
